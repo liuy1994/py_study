b796484f875e7f72a17161555062d146ab0286d1
liam <liam.liu@wayz.ai>
2022-07-07 15:49:27 +0800

    dict

--- list.py
+++ list.py
@@ -1,10 +1,9 @@
-# arr = [23, 123, 45, 656, [23]]
-# arr2 = [1,2,3]
-# arr.extend(arr2)
-# print(arr)
-arr = [1,2,3,4,1,2]
-# print(tup)
-# print(len(tup))
-arr.remove(1)
-arr.remove(1)
-print(arr)
+dict1 = {
+    "age": 23,
+    "name": "haha",
+
+}
+for key in dict1:
+    print(key)
+    print(dict1.get(key))
+    print("++++++++")
